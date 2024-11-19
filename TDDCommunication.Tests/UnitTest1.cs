77c24af591d5b90c667fdc44df21445be117f6bd
Samuel M <76839403+Syntrice@users.noreply.github.com>
2024-11-19 10:14:54 +0000

    Setup main project, test project and create .gitignore

---
+++ TDDCommunication.Tests/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace TDDCommunication.Tests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
