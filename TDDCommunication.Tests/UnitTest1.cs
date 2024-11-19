316ba3941de85acbeb04f087cdc16acc0aeda9d1
Agnes <133767448+agnes-vj@users.noreply.github.com>
2024-11-19 11:23:22 +0000

    update tests upto task3

--- TDDCommunication.Tests/UnitTest1.cs
+++ TDDCommunication.Tests/UnitTest1.cs
@@ -1,16 +1,74 @@
+
+using FluentAssertions;
+
 namespace TDDCommunication.Tests
 {
     public class Tests
     {
-        [SetUp]
-        public void Setup()
+
+
+        
+        [TestCase(new int[] { 3, 1, 2 }, 2, TestName = "Test with 3 numbers")]
+        [TestCase(new int[] { 3, 2 }, 0, TestName = "Test with 2 numbers")]
+        [TestCase(new int[] { 3 }, 0, TestName = "Test with 1 number")]
+        [TestCase(new int[] { 3,12, 1, 2,10 }, 15, TestName = "Test with more than 3 numbers")]
+
+        public void SumMiddleNumbersTest(int[] inputNumbers, int expectedOutput)
         {
+            //Arrange
+
+            Kata1 kata1 = new Kata1();
+
+            //Act
+            int actualOutput = kata1.SumMiddleNumbers(inputNumbers);
+
+            //Assert
+            actualOutput.Should().Be(expectedOutput);
         }
 
-        [Test]
-        public void Test1()
+        [TestCase(new int[] {6,5,4}, 5 , TestName = "Get mean of 3 numbers")]
+        [TestCase(new int[] { 40, 16, 5, 8 }, 17.25, TestName = "Get mean of more than 3 numbers")]
+        [TestCase(new int[] { 6,3,4 }, 4.33, TestName = "get mean of 3 numbers, rounded to decimal place")]
+
+
+
+
+        public void GetMeanOfNumbersTest(int[] inputNumbers, double expectedOutput)
         {
-            Assert.Pass();
+            //Arrange
+
+            Kata1 kata1 = new Kata1();
+
+            //Act
+            double actualOutput = kata1.GetMeanOfNumbers(inputNumbers);
+
+            //Assert
+            actualOutput.Should().Be(expectedOutput);
+
         }
+
+
+
+        [TestCase("northcoders", 1195, TestName = "sum of ASCII value of chars of a string")]
+        [TestCase("Northcoders", 1163, TestName = "sum of ASCII value of chars of a string")]
+        [TestCase("a", 97, TestName = "sum of ASCII value of chars of a string")]
+
+
+
+        public void GetMeanOfNumbersTest(string inputString, int expectedOutput)
+        {
+            //Arrange
+
+            Kata1 kata1 = new Kata1();
+
+            //Act
+            int actualOutput = kata1.GetSumOfASCII(inputString);
+
+            //Assert
+            actualOutput.Should().Be(expectedOutput);
+
+        }
+
+
     }
 }
---
+++ TDDCommunication/Kata1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TDDCommunication
+{
+    public class Kata1
+    {
+
+        public int SumMiddleNumbers(int[] numberArray)
+        {
+            int sum = 0;
+            Array.Sort(numberArray);
+            for (int i = 1; i < numberArray.Length - 1; i++)
+            {
+                sum += numberArray[i];
+            }
+
+            return sum;
+
+        }
+        public double GetMeanOfNumbers(int[] numberArray)
+        {
+            double sum = 0;
+
+            for (int i = 0;i < numberArray.Length; i++)
+            {
+                sum += numberArray[i];
+            }
+
+            return Math.Round((double)sum/ numberArray.Length, 2);
+            
+
+        }
+        public int GetSumOfASCII(string inputString)
+        {
+            int sum = 0;
+            foreach (char c in inputString)
+            {
+                sum += (int)c;
+            }
+
+            return sum;
+        }
+    }
+}
